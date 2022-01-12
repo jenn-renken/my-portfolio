441f7ed6c06c2fc85994f2cc0c2fbec878774657
jenn-renken <88343948+jenn-renken@users.noreply.github.com>
2022-01-12 11:24:08 -0500

    setting up components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import './index.css';
+import Home from './components/Home';
+import About from './components/About';
+import NavTabs from './components/NavTabs';
+import Portfolio from './components/Portfolio';
+import Contact from './components/Contact';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <NavTabs></NavTabs>
+      <main>
+        <Home></Home>
+        <Contact></Contact>
+        <Portfolio></Portfolio>
+        <About></About>
+      </main>
     </div>
   );
 }
