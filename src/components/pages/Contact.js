0a99f952c4190772032a14e2473139a9b10edeca
jenn-renken <88343948+jenn-renken@users.noreply.github.com>
2022-01-16 11:08:40 -0500

    fixed contact form bug

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -6,98 +6,88 @@ function Contact() {
     const [errorMessage, setErrorMessage] = useState("");
     const { name, email, message } = formState;
 
-    // function handleChange(e) {
-    //     if(e.currentTarget.name === 'email') {
-    //         const isValid = validateEmail(e.currentTarget.value);
-    //         console.log(isValid);
-    //         // is valid conditional statement
-    //         if (!isValid) {
-    //             setErrorMessage('Your email is invalid');
-    //         } else {
-    //             setErrorMessage('');
-    //         } 
-    //     } else {
-    //         if (!e.currentTarget.value.length) {
-    //             setErrorMessage(`${e.currentTarget.name} is required`);
-    //         } else {
-    //             setErrorMessage('');
-    //         }
-    //     }
-    //     if (!errorMessage) {
-    //         setFormState({ ...formState, [e.currentTarget.name]: e.currentTarget.value });
-    //     }
-    // }
-
-    useEffect(() => {
-      validate()
-    }, [formState])
-
-    function validate() {
-      let currentError = "";
-      if (formState.name.length <= 0) {
-        currentError+="name "
+  
+    function handleChange(e) {
+      if (e.target.name === "email") {
+        const isValid = validateEmail(e.target.value);
+        if (!isValid) {
+          setErrorMessage("Your email is invalid.");
+        } else {
+          if (!e.target.value.length) {
+            setErrorMessage(`${e.target.name} is required.`);
+          } else {
+            setErrorMessage("");
+          }
+        }
       }
-      if (formState.email.length <= 0 || !validateEmail(formState.email)) {
-        currentError+="email "
+  
+      if (!errorMessage) {
+        setFormState({ ...formState, [e.target.name]: e.target.value });
       }
-      if (formState.message.length <= 0) {
-        currentError+="message "
-      }
-      if (currentError.length <= 0) {
-        setErrorMessage("")
-      }
-      else {
-        setErrorMessage(currentError + "must be included")
-      }
-      
     }
-
-    function updateName(event) {
-      const name = event.currentTarget.value;
-      setFormState({...formState, name })
-    }
-
-    function updateEmail(event) {
-      const email = event.currentTarget.value;
-      setFormState({...formState, email })
-    }
-
-    function updateMessage(event) {
-      const message = event.currentTarget.value;
-      setFormState({...formState, message })
-    }
-
-
-
-    function handleSubmit(e) {
-        e.preventDefault();
-        console.log(formState);
+  
+    function handleBlank(e) {
+      if (e.target.name === "Name" || e.target.name === "Message") {
+        if (!e.target.value.length) {
+          setErrorMessage(`${e.target.name} is required.`);
+        } else {
+          setErrorMessage("");
+        }
+      }
+  
+      if (!errorMessage) {
+        setFormState({ ...formState, [e.target.name]: e.target.value });
+      }
     }
 
     return (
         <section>
           <div className="container is-max-desktop">
             <div className='title is-4 has-text-link'>Use this form to send me a message!</div>   
-              <form id="contact-form" onSubmit={handleSubmit}>
+              <form id="contact-form">
                   <div className='field'>
-                      <label htmlFor='name'className='label'>Name:</label>
+                      <label htmlFor='Name' className='label'>Name:</label>
                       <div className='control'>
-                        <input type="text" className='input' placeholder="Name" defaultValue={name} onChange={updateName} />
+                        <input 
+                          type="text" 
+                          className='input' 
+                          placeholder="Name" 
+                          defaultValue={name} 
+                          onBlur={handleBlank} 
+                          name="Name"
+                        />
                       </div>
                       
                   </div>
                   <div className='field'>
                       <label htmlFor="email" className='label'>Email address:</label>
-                      <input className="input" type="email" placeholder="Email" defaultValue={email} onChange={updateEmail} />
+                      <input 
+                        className="input" 
+                        type="email" 
+                        placeholder="Email" 
+                        onBlur={handleChange}
+                        name='email'
+                      />
                   </div>
                   <div className='field'>
-                      <label htmlFor="message" className='label'>Message:</label>
-                      <textarea  className="textarea is-medium" placeholder="Your message" rows={5} defaultValue={message} onChange={updateMessage}/>
-                      <p>{errorMessage}</p>
-                      <div className='buttons'></div>
+                      <label htmlFor="Message" className='label'>Message:</label>
+                      <textarea  
+                        className="textarea is-medium" 
+                        name='Message'
+                        placeholder="Your message" 
+                        rows={5} 
+                        defaultValue={message} 
+                        onBlur={handleBlank}
+                      />
+               </div>
+                  {errorMessage && (
+                      <div>
+                          <p className="error-text">{errorMessage}</p>
+                      </div>
+                  )}
+                      <div className='buttons'>
                         <button className="button is-link is-light" type="submit">Submit</button>
-          
-                  </div>
+                      </div>
               </form>
             </div>
         </section>
